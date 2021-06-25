dd77d58a319dd822608b3c0a92e7c3fc7e709510
jeremyp1ttard <84363943+jeremyp1ttard@users.noreply.github.com>
2021-06-25 15:15:29 +0800

    tweaks to test, additional json added to be used for cookies

--- scenarios/inte/product-click--mobile.js
+++ scenarios/inte/product-click--mobile.js
@@ -1,5 +1,5 @@
 module.exports = {
-    label: "Inte Product Click",
+    label: "Inte Product Click Mobile",
     url: "https://inte.cashconverters.com.au/c3api/product/lookup?barcode=010800000040",
     referenceUrl: "",
     readyEvent: "",
@@ -8,24 +8,19 @@ module.exports = {
     hideSelectors: [],
     removeSelectors: [],
     hoverSelector: "",
-    clickSelector: ".btn--add-to-cart", //wont work if less specific...
+    clickSelector: ".toolbar .toolbar__buy .btn--add-to-cart", //wont work if less specific...
     postInteractionWait: 1000,
     selectors: [],
     selectorExpansion: true,
     expect: 0,
     misMatchThreshold: 0.1,
     requireSameDimensions: true,
-    viewports: [ //phone errors out obn click
-        {
-          "label": "tablet",
-          "width": 1024,
-          "height": 768
-        },
-        {
-          "label": "desktop",
-          "width": 1920,
-          "height": 1080
-        }
-      ]
+    viewports: [
+      {
+        "label": "phone",
+        "width": 320,
+        "height": 480
+      },
+    ]
   };
   
